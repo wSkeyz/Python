d6e4949348ad484c7fbf02866129d67e0e08e8b0
Alexs <sanderrodrigues00@gmail.com>
2025-10-15 17:34:05 -0300

    Desafio 36

--- Exercicios python Curso emVideo/aula.py
+++ Exercicios python Curso emVideo/aula.py
@@ -1,3 +1,9 @@
-import random
-random.random(1, 2
-)
+nome = str(input("Qual e seu nome?")).strip().capitalize()
+if nome == "Alex":
+    print("Bom dia nome bonito")
+elif nome == "Fran":
+    print("Nome da minha namorada")
+elif nome in "Ana maria braga":
+    print("Voce e do progama de tv?")
+else:
+    print("Seu nome e {}".format(nome))     
