1f2078ad467eca0ac30adbd3a2b78f7e5d70eca0
Alexs <sanderrodrigues00@gmail.com>
2025-10-17 20:17:40 -0300

    Add opção

--- Exercicios python Curso emVideo/desafio37.py
+++ Exercicios python Curso emVideo/desafio37.py
@@ -1,6 +1,10 @@
 Numero = int(input("Digite um numero inteiro:"))
 Base = int(input ("Escolha uma Opção para conversão \n 1.Binario \n 2.Octal \n 3.Hexadecimal \n Opção:" ))
-if Base == 1:
+
+if Base >= 4:
+    print("Opção invalida. Tente novamente.")
+
+elif Base == 1:
     print("Numero {} em Binario e {}".format(Numero, bin(Numero)[2:]))
 elif Base == 2:
     print("Numero {} em Octal  e {}".format(Numero, oct(Numero)[2:]))
