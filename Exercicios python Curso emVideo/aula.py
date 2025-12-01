de6c284411a0ef4dd163af95d57e85126fb850e1
Alexs <sanderrodrigues00@gmail.com>
2025-12-01 20:20:21 -0300

    Update aula.py

--- Exercicios python Curso emVideo/aula.py
+++ Exercicios python Curso emVideo/aula.py
@@ -1,2 +1,7 @@
-lanche = ("Hamburgue", "Pudim", "Suco", "Pizza")
-print (lanche[3])
+c = float(input("Capital inicial:"))
+i = float(input("Qual a taxa de juros anual?"))
+i = i / 100
+t = int(input("Quantos anos?"))
+# t = t * 12
+m = c * (1+ i) ** t
+print(f"Final: {m:.2f}")
